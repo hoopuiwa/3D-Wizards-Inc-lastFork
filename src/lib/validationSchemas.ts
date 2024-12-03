f168daed398180dd515dd688d8f9844a2d972d04
Minkyeong Kim <kim83@hawaii.edu>
2024-12-02 18:15:01 -1000

    new custom order

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -1,5 +1,4 @@
 import * as Yup from 'yup';
-import { Option, Size, Color } from '@prisma/client';
 
 export const AddStuffSchema = Yup.object({
   name: Yup.string().required(),
@@ -16,30 +15,3 @@ export const EditStuffSchema = Yup.object({
   owner: Yup.string().required(),
 });
 
-// Define the TypeScript interface for the form data
-export interface ICreateProductForm {
-  id?: number;
-  quantity: number;
-  owner: string;
-  option: Option;
-  size: Size;
-  color: Color[];
-}
-
-export const CreateProductSchema = Yup.object({
-  option: Yup.mixed<Option>()
-    .oneOf(Object.values(Option))
-    .required('Option is required'),
-  size: Yup.mixed<Size>()
-    .oneOf(Object.values(Size))
-    .required('Size is required'),
-  color: Yup.array()
-    .of(Yup.mixed<Color>().oneOf(Object.values(Color)).required())
-    .min(1, 'At least one color is required') // Ensures at least one color is selected
-    .required('Color is required'),
-  quantity: Yup.number()
-    .required('Quantity is required')
-    .positive('Quantity must be a positive number')
-    .integer('Quantity must be an integer'),
-  owner: Yup.string().required('Owner is required'),
-});
