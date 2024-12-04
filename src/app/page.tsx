94b2adc2bdd4d79c21ac55dc86ea48849b6e3c26
Minkyeong Kim <kim83@hawaii.edu>
2024-12-03 15:01:15 -1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,15 +21,19 @@ const Home = () => {
         </Row>
         <Row className="align-middle text-center">
           <Col xs={4}>
-            <h1>Catchy Headline and information!</h1>
+            <h1>With more than 100 printers running 24/7, we’re the largest printer farm in Hawaii!</h1>
           </Col>
 
           <Col xs={4}>
-            <h1>Other Catchy Headline and information!</h1>
+            <h1>
+              With more than a decade and a half of combined 3d printing knowledge,
+              we have expertise that we’re excited to share!
+              {' '}
+            </h1>
           </Col>
 
           <Col xs={4}>
-            <h1> 3rd Catchy Headline and information!!!</h1>
+            <h1> Find us in person at 50+ events around the island every month, use code #NewWebsite to get %10 off</h1>
           </Col>
         </Row>
       </Container>
