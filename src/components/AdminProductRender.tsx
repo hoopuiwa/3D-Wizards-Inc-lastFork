33763db61050a485386183002687a526488d2cf5
hoopuiwa <puiwap1@gmail.com>
2024-12-10 23:09:56 -1000

    admin page update
    on admin tab ->
    - should display checked out products grouped by user
    - should display all users aswell

---
+++ src/components/AdminProductRender.tsx
@@ -0,0 +1,17 @@
+import { Product } from '@prisma/client';
+
+/* Renders a single row in the List Stuff table. See list/page.tsx. */
+const StuffItemAdmin = ({ option, size, quantity, color1, color2, color3 }: Product) => (
+  <tr>
+    {/* empty element to sorta shift row right */}
+    <td>{ }</td>
+    <td>{option}</td>
+    <td>{size}</td>
+    <td>{color1}</td>
+    <td>{color2}</td>
+    <td>{color3}</td>
+    <td>{quantity}</td>
+  </tr>
+);
+
+export default StuffItemAdmin;
