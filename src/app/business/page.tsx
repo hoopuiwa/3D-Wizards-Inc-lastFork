405d7d3ea1938a854a287d28c389ce17efb8fedd
Minkyeong Kim <kim83@hawaii.edu>
2024-12-15 15:56:26 -1000

    update page.tsx

--- src/app/business/page.tsx
+++ src/app/business/page.tsx
@@ -9,12 +9,14 @@ const BusinessPage = async () => (
           <h1 className="py-4">Why Choose Us?</h1>
           <h5>
             If your business requires regularly produced 3D printed products,
-            you may be in luck. We already have several clients that can vouch
-            for our reliability and the quality of our product.
+            you may be in luck.
+            <br />
+            We already have several clients that can vouch for our reliability and the quality of our product.
             <br />
             A benefit of working with a local 3D printing service is that overseas
             shipping times and costs arent a factor to overcome, simply wait for us to
             drive over from Ewa at a schedule of your preference.
+            <br />
             <br />
             Please reach out at
             the following email if you’d like more information including,
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -9,7 +9,7 @@ const Gallery = () => {
     { id: 3, url: '/images/Gall_3.jpg' },
     { id: 4, url: '/images/Gall_4.jpg' },
     { id: 5, url: '/images/Gall_5.jpg' },
-    { id: 6, url: '/images/Gall_5.jpg' },
+    { id: 6, url: '/images/Gall_6.jpg' },
     { id: 7, url: '/images/Gall_7.jpg' },
     { id: 8, url: '/images/Gall_8.jpg' },
     { id: 9, url: '/images/Gall_9.jpg' },
@@ -24,14 +24,15 @@ const Gallery = () => {
         <Row className="p-4 align-middle text-center">
           <Col>
             <h1 className="mb-3">Gallery</h1>
-            <h5 className="mb-5">
+            <br />
+            <h6 className="mb-5">
               <p>
                 We attend over 50 events per month and have a lot of fun doing so.
                 We also regularly work on complicated custom orders.
                 <br />
                 Check out what we’ve been up to
               </p>
-            </h5>
+            </h6>
           </Col>
         </Row>
         <Row xs={2} md={3} lg={4} className="g-4">
