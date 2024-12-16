51729db5f42742cee67849d1fa1b62b9e90d3488
Minkyeong Kim <kim83@hawaii.edu>
2024-12-15 15:20:56 -1000

    Update page.tsx

--- src/app/business/page.tsx
+++ src/app/business/page.tsx
@@ -6,19 +6,21 @@ const BusinessPage = async () => (
     <Container id="about-us" fluid className="py-3">
       <Row className="justify-content-center align-items-center m-3">
         <Col md={8}>
-          <h3 className="py-4">Business Inquiries</h3>
+          <h2 className="py-4">Why Choose Us?</h2>
           <p>
             If your business requires regularly produced 3D printed products,
             you may be in luck. We already have several clients that can vouch
             for our reliability and the quality of our product.
-            A benefit of working with a local 3D printing service is that oversea
+          </p>
+          <p>
+            A benefit of working with a local 3D printing service is that overseas
             shipping times and costs arent a factor to overcome, simply wait for us to
             drive over from Ewa at a schedule of your preference. Please reach out at
-            the following email if you’d like more information includin,
+            the following email if you’d like more information including,
             but not limited to, our wholesale prices.
           </p>
           <p>
-            3dwizardsinc@gmail.com
+            <a href="mailto:3dwizardsinc@gmail.com">3dwizardsinc@gmail.com</a>
           </p>
         </Col>
       </Row>
